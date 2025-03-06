da6aeaef1c9c5a56ddb28dd1d2c6c17430d1f930
Julu <julu21051998panda@gmail.com>
2025-03-07 01:35:18 +0530

    Update learn.sql

--- learn.sql
+++ learn.sql
@@ -4,8 +4,9 @@ CREATE TABLE cars (
   year INT
 );
 
-INSERT INTO cars ( BMW,M4,2008
-MERCEDES,A5,2009
-AUDI,R8,2002);
+INSERT INTO cars (brand,model,year) 
+  VALUES( 'BMW','M4',2008
+'MERCEDES','A5',2009
+'AUDI','R8',2002);
 
 select * from cars;
