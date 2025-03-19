3a0f2b3ebece1e5087fe1cb7a4ad25c314dd437b
Julu <julu21051998panda@gmail.com>
2025-03-19 22:27:17 +0530

    Update learn.sql

--- learn.sql
+++ learn.sql
@@ -1,12 +1,3 @@
-CREATE TABLE cars (
-  brand VARCHAR(255),
-  model VARCHAR(255),
-  year INT
-);
-
-INSERT INTO cars (brand, model, year)
-VALUES
-  ('Volvo', 'p1800', 1968),
-  ('BMW', 'M1', 1978),
-  ('Toyota', 'Celica', 1975);
-
+mode.sql
+files.sql
+trevor.sql
