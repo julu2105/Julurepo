9a8d0c76497c3865aead4df1833c1aaffa304559
Julu <julu21051998panda@gmail.com>
2025-04-10 16:19:51 +0530

    Update trevor.sql

--- trevor.sql
+++ trevor.sql
@@ -1,1 +1,13 @@
-select * from information_schema.tables LIMIT 15
+CREATE TABLE contacts (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(384) NOT NULL UNIQUE
+);
+
+
+INSERT INTO contacts (first_name, last_name, email)
+VALUES
+    ('John', 'Doe', 'john.doe@example.com'),
+    ('Jane', 'Smith', 'jane.smith@example.com'),
+    ('Bob', 'Johnson', 'bob.johnson@example.com');
