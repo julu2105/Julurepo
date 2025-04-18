3ec6758182153460cd4807e9acfeaf916c1e9419
Julu <julu21051998panda@gmail.com>
2025-04-18 14:57:14 +0530

    Update index.js

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const body-parser = require('body-parser');
 
+const app = express();
+const port = 3000
+app.use.(body-parser.json());
+
+app.get('/',(req, res) => {
+  res.send('Hello World');
+});
